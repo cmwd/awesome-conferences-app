9be3ccc30db8de307b2db532a4a8048cb37514e5
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2016-08-29 21:30:14 +0200

    [API] conferences controller cleanup

--- src/api/src/controller/conferences-controller.js
+++ src/api/src/controller/conferences-controller.js
@@ -2,6 +2,17 @@ const co = require('co');
 const paginator = require('../utils/paginator');
 const { conferenceModel } = require('../model/index');
 
+function *getConferencesList(offset, limit) {
+  const conferences = yield conferenceModel
+    .find()
+    .skip(offset)
+    .limit(limit);
+  const count = yield conferenceModel.count();
+  const pages = paginator.generate(count, offset, limit);
+
+  return { pages, conferences };
+}
+
 function getConferences(req, res, next) {
   let {
     limit = 20,
@@ -11,16 +22,7 @@ function getConferences(req, res, next) {
   offset = parseInt(offset, 10);
   limit = parseInt(limit, 10);
 
-  co(function *getConferencesData() {
-    const conferences = yield conferenceModel
-      .find()
-      .skip(offset)
-      .limit(limit);
-    const count = yield conferenceModel.count();
-    const pages = paginator.generate(count, offset, limit);
-
-    return { pages, conferences };
-  })
+  co(getConferencesList(offset, limit))
   .then(data => res.json(data))
   .catch(next);
 }
