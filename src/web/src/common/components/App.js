9d72053d98d4d57f67f3fd67eb6383227b58734d
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2016-09-24 20:51:53 +0200

    Add details index

--- src/web/src/common/components/App.js
+++ src/web/src/common/components/App.js
@@ -3,6 +3,7 @@ import { Match, Redirect, Miss } from 'react-router';
 import { Grid, Row, Col } from './Bootstrap';
 import { Header, NoMatch } from './index';
 import { VisibleConferences } from '../containers';
+import Details from './details/DetailsIndex';
 
 const GoToConferences = () => (
   <Redirect to="/page/1" />
@@ -16,9 +17,8 @@ const App = () => (
       </Col>
     </Row>
     <Match pattern="/" exactly component={GoToConferences} />
-    <Match
-      pattern="/page/:current" component={VisibleConferences}
-    />
+    <Match pattern="/page/:current" component={VisibleConferences} />
+    <Match pattern="/details/:conferenceId" component={Details} />
     <Miss component={NoMatch} />
   </Grid>
 );
---
+++ src/web/src/common/components/details/DetailsIndex.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DetailsIndex = ({ params }) => (
+  <div>Details {params.conferenceId}</div>
+);
+
+export default DetailsIndex;
