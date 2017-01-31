09be6d2ea95fe06b230a54122239d952a934683e
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2017-01-31 19:34:55 +0100

    Cleanup API config (#33)

--- src/api/config.js
+++ src/api/config.js
@@ -1,12 +1,13 @@
 const {
   APP_PORT = 8002,
   NODE_ENV = 'development',
-  COLLECTORS_ADDRESS = 'http://collectors:8001',
   DB_NAME = 'acl',
   DB_ADDRESS = 'mongodb://web.awc.local:27017',
-  TOKEN_SECRET = 'SUPER_IMPORTANT_TO_OVERRIDE_IN_PROD_ENV',
 } = process.env;
 
 module.exports = {
-  APP_PORT, COLLECTORS_ADDRESS, NODE_ENV, DB_NAME, DB_ADDRESS, TOKEN_SECRET,
+  APP_PORT,
+  NODE_ENV,
+  DB_NAME,
+  DB_ADDRESS,
 };
